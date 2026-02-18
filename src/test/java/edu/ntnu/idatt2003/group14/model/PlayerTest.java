8051508daf3076d7c496da1f4ef8702f128e22ae
Kevin Holswilder <kevinoh@stud.ntnu.no>
2026-02-18 13:39:09 +0100

    test: add test for Player class

--- src/test/java/edu/ntnu/idatt2003/group14/model/PlayerTest.java
+++ src/test/java/edu/ntnu/idatt2003/group14/model/PlayerTest.java
@@ -1,4 +1,48 @@
 package edu.ntnu.idatt2003.group14.model;
 
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Class responsible for testing the {@link Player} class.
+ *
+ * @author Kevin Holswilder
+ * @since 0.0.1
+ */
 public class PlayerTest {
+  private Player player;
+
+  @BeforeEach
+  public void instantiate_player() {
+    player = new Player(
+        "Bernt",
+        BigDecimal.valueOf(1000.0)
+    );
+  }
+
+  @Test
+  public void verify_player_name() {
+    Assertions.assertEquals("Bernt", player.getName());
+  }
+
+  @Test
+  public void verify_player_money() {
+    Assertions.assertEquals(BigDecimal.valueOf(1000.0), player.getMoney());
+  }
+
+  @Test
+  public void verify_deposit() {
+    player.depositMoney(BigDecimal.valueOf(100.0));
+    Assertions.assertEquals(BigDecimal.valueOf(1100.0), player.getMoney());
+  }
+
+  @Test
+  public void verify_withdraw() {
+    player.withdrawMoney(BigDecimal.valueOf(200.0));
+    Assertions.assertEquals(BigDecimal.valueOf(800.0), player.getMoney());
+  }
+
+  // TODO: Add more tests.
 }
