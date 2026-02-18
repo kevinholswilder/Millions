2af9308425fe74d641b03723e18811aa8e5f409d
Kevin Holswilder <kevinoh@stud.ntnu.no>
2026-02-18 11:45:52 +0100

    feat: add InsufficientBalanceException
    This introduces a new exception thrown upon depositing or withdrawing an equal or less than 0 value.

---
+++ src/main/java/edu/ntnu/idatt2003/group14/exception/InsufficientBalanceException.java
@@ -0,0 +1,20 @@
+package edu.ntnu.idatt2003.group14.exception;
+
+/**
+ * Exception thrown when a player tries to buy shares with insufficient funds.
+ *
+ * @author Kevin Holswilder
+ * @since 0.0.1
+ */
+public class InsufficientBalanceException extends Exception {
+
+  /**
+   * Constructor for InsufficientBalanceException.
+   *
+   * @param message the message
+   */
+  public InsufficientBalanceException(String message) {
+    super(message);
+  }
+
+}
