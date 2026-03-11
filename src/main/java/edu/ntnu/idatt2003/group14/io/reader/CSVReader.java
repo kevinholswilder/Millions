59416a9ae3feff827250d7dd23a51c12fd97fabd
Kevin Holswilder <kevinoh@stud.ntnu.no>
2026-03-11 09:56:48 +0100

    refactor(io): support reading files from the file system or internal resources folder

--- src/main/java/edu/ntnu/idatt2003/group14/io/reader/CSVReader.java
+++ src/main/java/edu/ntnu/idatt2003/group14/io/reader/CSVReader.java
@@ -2,9 +2,11 @@ package edu.ntnu.idatt2003.group14.io.reader;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Represents a class for reading CSV files.
@@ -15,37 +17,30 @@ import java.util.Objects;
 @SuppressWarnings("checkstyle:AbbreviationAsWordInName")
 public class CSVReader {
 
-  private static final String SEPARATOR = ",";
-  private static final String COMMENT_CHAR = "#";
-
-  public static String getSeparator() {
-    return SEPARATOR;
-  }
-
-  public static String getCommentChar() {
-    return COMMENT_CHAR;
-  }
-
   /**
-   * Reads a file from the resources folder.
+   * Reads a file either from the local system or from the resources folder.
    *
    * @param filename the name of the file to read
    * @return a list of lines from the file
    * @throws IOException if the file cannot be read
    */
   public List<String> readFromFile(String filename) throws IOException {
-    try (
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(
-                Objects.requireNonNull(getClass().getResourceAsStream("/" + filename)))
-        )
-    ) {
-      if (br.ready()) {
+    Path path = Path.of(filename);
+
+    if (Files.exists(path)) {
+      try (BufferedReader br = Files.newBufferedReader(path)) {
         return br.readAllLines();
       }
-    } catch (NullPointerException e) {
-      System.err.println("Could not find file: " + filename + " in resources folder.");
     }
-    return null;
+
+    try (InputStream is = getClass().getResourceAsStream("/" + filename)) {
+      if (is == null) {
+        throw new IOException("File not found: " + filename);
+      }
+
+      try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+        return br.readAllLines();
+      }
+    }
   }
 }
