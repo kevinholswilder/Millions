77fc2acf458f4f418ff6965d72d16296bc200437
Elias Haugsbakk <elias.haugsbakk@gmail.com>
2026-04-09 14:24:08 +0200

    feat: create ui/App as JavaFX starting point
    Edit pom.xml to build jar with dependencies.
    Change Main to Launcher

--- src/main/java/edu/ntnu/idatt2003/group14/Launcher.java
+++ src/main/java/edu/ntnu/idatt2003/group14/Launcher.java
@@ -1,28 +1,31 @@
 package edu.ntnu.idatt2003.group14;
 
+import edu.ntnu.idatt2003.group14.ui.App;
 import edu.ntnu.idatt2003.group14.utils.AppDataPathUtil;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Application;
 
 /**
- * Main class: the entrypoint of the program.
+ * Launcher class: the entrypoint of the program.
  *
  * @author Elias Haugsbakk, Kevin Holswilder
  * @since 0.0.1
  */
-public class Main {
+public class Launcher {
 
   public static final Logger LOGGER = Logger.getLogger("Millions");
 
   /**
    * Entrypoint for the application.
    *
-   * <p>Initializes the data directory path for Millions.</p>
+   * <p>Initializes the data directory path for Millions and starts the JavaFX application.</p>
    */
-  static void main() {
+  void main(String[] args) {
     try {
       AppDataPathUtil.initializeDataDirectory();
+      Application.launch(App.class, args);
     } catch (IOException e) {
       LOGGER.log(Level.SEVERE, "Failed to initialize data directory.", e);
     }
