d8de1977d5ebe4eebf8ab532b25bef308df2e486
Elias Haugsbakk <elias.haugsbakk@gmail.com>
2026-04-20 16:04:27 +0200

    chore: add skipMenu launch option

--- src/main/java/edu/ntnu/idatt2003/group14/ui/App.java
+++ src/main/java/edu/ntnu/idatt2003/group14/ui/App.java
@@ -5,6 +5,7 @@ import edu.ntnu.idatt2003.group14.ui.app.AppController;
 import edu.ntnu.idatt2003.group14.ui.app.AppControllerImpl;
 import edu.ntnu.idatt2003.group14.ui.app.AppNavigator;
 import edu.ntnu.idatt2003.group14.ui.app.AppNavigatorImpl;
+import java.util.List;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -13,9 +14,24 @@ import javafx.stage.Stage;
  * the necessary classes and starting the application.
  */
 public final class App extends Application {
+  private boolean skipMenu = false;
+
+  @Override
+  public void init() {
+    Parameters params = getParameters();
+    List<String> raw = params.getRaw();
+    skipMenu = raw.contains("--skipMenu");
+
+    if (skipMenu) {
+      debugPrint("Skipping menu screen");
+      debugPrint("User: \"TestUser\"");
+      debugPrint("Stocks file: \"/resources/sp500.csv\"");
+    }
+  }
 
   @Override
   public void start(Stage stage) {
+
     stage.setTitle("Millions");
 
     AudioManager audioManager = new AudioManager();
@@ -23,8 +39,23 @@ public final class App extends Application {
 
     AppController appController = new AppControllerImpl(stage);
     AppNavigator appNavigator = new AppNavigatorImpl(stage, appController, audioManager);
-    appNavigator.showMainMenuView();
 
+    if (skipMenu) {
+      skipMenu(appNavigator);
+    } else {
+      appNavigator.showMainMenuView();
+    }
     stage.show();
+  }
+
+  private void skipMenu(AppNavigator appNavigator) {
+    // enable by running with:
+    // javafx:run -Djavafx.args=--skipMenu
+    // java -Djavafx.args="--skipMenu" -jar Millions.jar
+    appNavigator.showPortfolioView();
+  }
+
+  private static void debugPrint(String message) {
+    IO.println("\u001B[33m[DEBUG]\u001B[38;5;15m " + message + "\u001B[0m");
   }
 }
