3a2d6e53838938efcd5f458d06b3ec43b0972c08
Kevin Holswilder <kevinoh@stud.ntnu.no>
2026-04-23 11:49:22 +0200

    feat: add TransactionArchive controller logic

---
+++ src/main/java/edu/ntnu/idatt2003/group14/ui/transaction/TransactionArchiveController.java
@@ -0,0 +1,28 @@
+package edu.ntnu.idatt2003.group14.ui.transaction;
+
+import edu.ntnu.idatt2003.group14.model.Share;
+import edu.ntnu.idatt2003.group14.model.Stock;
+import edu.ntnu.idatt2003.group14.model.transaction.Transaction;
+import javafx.scene.layout.HBox;
+
+/**
+ *
+ * @author Kevin Holswilder
+ * @since 0.0.1
+ */
+public class TransactionArchiveController {
+
+    /**
+     * Creates a row for a transaction.
+     *
+     * @param transaction
+     * @return
+     */
+    public HBox createTransactionRow(Transaction transaction) {
+        Share share = transaction.getShare();
+        Stock stock = share.getStock();
+
+        return new HBox(16);
+    }
+
+}
