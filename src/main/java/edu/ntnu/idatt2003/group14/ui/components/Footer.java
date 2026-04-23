2418363da3a168c52409f6d343d8a6a369644366
Kevin Holswilder <kevinoh@stud.ntnu.no>
2026-04-23 11:48:28 +0200

    feat: implement Footer component

---
+++ src/main/java/edu/ntnu/idatt2003/group14/ui/components/Footer.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idatt2003.group14.ui.components;
+
+import javafx.scene.control.Button;
+
+public class Footer extends Button {
+
+    public Footer() {
+        this.setText("Settings");
+        this.setMaxWidth(Double.MAX_VALUE);
+        this.setPrefHeight(100.0);
+    }
+
+}
