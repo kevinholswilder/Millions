88798df888bbbef6b20d84b2532e5acddb90ba79
Kevin Holswilder <kevinoh@stud.ntnu.no>
2026-04-23 11:49:28 +0200

    feat: create TransactionArchiveView UI

---
+++ src/main/java/edu/ntnu/idatt2003/group14/ui/transaction/TransactionArchiveView.java
@@ -0,0 +1,44 @@
+package edu.ntnu.idatt2003.group14.ui.transaction;
+
+import edu.ntnu.idatt2003.group14.model.transaction.Transaction;
+import edu.ntnu.idatt2003.group14.model.transaction.TransactionArchive;
+
+import java.util.List;
+
+import edu.ntnu.idatt2003.group14.ui.app.AppController;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author Kevin Holswilder
+ * @since 0.0.1
+ */
+public class TransactionArchiveView {
+    private final StackPane root;
+    private final TransactionArchiveController controller;
+
+    /**
+     *
+     */
+    public TransactionArchiveView(TransactionArchiveController controller, AppController appController) {
+        this.root = new StackPane();
+        this.controller = controller;
+
+        root.setPadding(new Insets(20));
+
+        TransactionArchive archive = new TransactionArchive();
+        List<Transaction> transactions = archive.getTransactions(1);
+
+        VBox list = new VBox(10);
+        for (Transaction transaction : transactions) {
+            list.getChildren().add(controller.createTransactionRow(transaction));
+        }
+    }
+
+    public Parent getRoot() {
+        return root;
+    }
+}
