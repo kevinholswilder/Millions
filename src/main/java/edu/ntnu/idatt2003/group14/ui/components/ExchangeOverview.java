e2d8698be276daf863edd502cac75c6e1c22796f
Kevin Holswilder <kevinoh@stud.ntnu.no>
2026-05-08 13:55:48 +0200

    chore: add necessary javadoc to comply with CheckStyle

--- src/main/java/edu/ntnu/idatt2003/group14/ui/components/ExchangeOverview.java
+++ src/main/java/edu/ntnu/idatt2003/group14/ui/components/ExchangeOverview.java
@@ -3,15 +3,22 @@ package edu.ntnu.idatt2003.group14.ui.components;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
+/**
+ * A vertical list of exchange buttons.
+ *
+ * @author Kevin Holswilder
+ */
 public class ExchangeOverview extends VBox {
 
-    public ExchangeOverview() {
-        for (int i = 0; i < 10; i++) {
-            Button exchangeButton = new Button("S&P 500");
-            exchangeButton.setMaxWidth(Double.MAX_VALUE);
-            exchangeButton.setPrefHeight(100);
-            this.getChildren().add(exchangeButton);
-        }
+  /**
+   * Creates the exchange overview.
+   */
+  public ExchangeOverview() {
+    for (int i = 0; i < 10; i++) {
+      Button exchangeButton = new Button("S&P 500");
+      exchangeButton.setMaxWidth(Double.MAX_VALUE);
+      exchangeButton.setPrefHeight(100);
+      this.getChildren().add(exchangeButton);
     }
-
+  }
 }
